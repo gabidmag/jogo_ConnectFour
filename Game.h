6b7622d5bfdaabe164bd5ffa58f1626e91dcee48
pedromatos007 <pedhenma1@gmail.com>
2025-10-15 15:27:52 -0300

    Primeira versão da lógica do jogo

--- Game.h
+++ Game.h
@@ -4,13 +4,18 @@
 #define BOARD_ROWS 6
 #define BOARD_COLS 7
 
-#define EMPTY_CELL   '.'
-#define PLAYER_PIECE 'X'
-#define IA_PIECE     'O'
+#define EMPTY_CELL    '.'
+#define PLAYER1_PIECE 'X'
+#define PLAYER2_PIECE 'O'
+
+typedef enum {
+    MODE_PVP,
+    MODE_PVIA
+} GameMode;
 
 void initialize_board(char board[BOARD_ROWS][BOARD_COLS]);
 void display_board(char board[BOARD_ROWS][BOARD_COLS]);
-int get_player_move(char board[BOARD_ROWS][BOARD_COLS]);
+int get_human_move(char board[BOARD_ROWS][BOARD_COLS], char current_piece);
 int get_ia_move(char board[BOARD_ROWS][BOARD_COLS]);
 int drop_piece(char board[BOARD_ROWS][BOARD_COLS], int col, char piece);
 int check_win(char board[BOARD_ROWS][BOARD_COLS], char piece);
