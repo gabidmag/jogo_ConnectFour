5d42be123ddab10a3446c584d66574ac9d702891
pedromatos007 <pedhenma1@gmail.com>
2025-10-15 15:16:22 -0300

    Primeira versão da lógica do jogo
    definições de constantes e os protótipos das funções que serão usadas na logica do jogo.

---
+++ Game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+
+#define BOARD_ROWS 6
+#define BOARD_COLS 7
+
+#define EMPTY_CELL   '.'
+#define PLAYER_PIECE 'X'
+#define IA_PIECE     'O'
+
+void initialize_board(char board[BOARD_ROWS][BOARD_COLS]);
+void display_board(char board[BOARD_ROWS][BOARD_COLS]);
+int get_player_move(char board[BOARD_ROWS][BOARD_COLS]);
+int get_ia_move(char board[BOARD_ROWS][BOARD_COLS]);
+int drop_piece(char board[BOARD_ROWS][BOARD_COLS], int col, char piece);
+int check_win(char board[BOARD_ROWS][BOARD_COLS], char piece);
+int check_draw(char board[BOARD_ROWS][BOARD_COLS]);
+void run_game();
+
+#endif
